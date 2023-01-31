2296b586a18056faa1bc6f64e6c4ebefbe0779f3
Oussama95el <oussamaelbechari@gmail.com>
2023-01-31 15:53:00 +0100

    pushing dist folder

--- bank_connect/src/main/java/com/simplon/bank_connect/compte/professionnel/ProfessionelRepository.java
+++
@@ -1,8 +0,0 @@
-package com.simplon.bank_connect.compte.professionnel;
-
-import com.simplon.bank_connect.compte.CompteRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProfessionelRepository extends CompteRepository {
-}
--- bank_connect/src/main/java/com/simplon/bank_connect/compte/professionnel/ProfessionelService.java
+++
@@ -1,8 +0,0 @@
-package com.simplon.bank_connect.compte.professionnel;
-
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class ProfessionelService {
-}
